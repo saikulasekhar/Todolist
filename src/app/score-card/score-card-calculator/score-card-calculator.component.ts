851ec62ab48ede75ba221f7a13602011fd13fb2c
saikulasekhar <sekharsairoyal@gmail.com>
2019-10-28 11:21:21 +0530

    result function written

--- src/app/score-card/score-card-calculator/score-card-calculator.component.ts
+++ src/app/score-card/score-card-calculator/score-card-calculator.component.ts
@@ -7,50 +7,52 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./score-card-calculator.component.css']
 })
 export class ScoreCardCalculatorComponent implements OnInit {
-  teamA={
-    teamA:[{
-    playerName :null,
-    runs:null,
-    boundaries:null,
-    wickets: null,
-    oversPlayed:null,
-    oversBowled:null,
-  }],
+  teamA = {
+    teamA: [{
+      playerName: '',
+      runs: null,
+      boundaries: null,
+      wickets: null,
+      oversPlayed: null,
+      oversBowled: null,
+    }],
     totalRuns: null,
-    totalBoundaries:null,
-    totalOversPlayed:null,
-    totalOversBowled:null,
-    totalWickets:null,
+    totalBoundaries: null,
+    totalOversPlayed: null,
+    totalOversBowled: null,
+    totalWickets: null,
+    result: '',
   };
 
-  teamB={
-    teamB:[{
-    playerName :null,
-    runs:null,
-    boundaries:null,
-    wickets: null,
-    oversPlayed:null,
-    oversBowled:null,
-  }],
+  teamB = {
+    teamB: [{
+      playerName: '',
+      runs: null,
+      boundaries: null,
+      wickets: null,
+      oversPlayed: null,
+      oversBowled: null,
+    }],
     totalRuns: null,
-    totalBoundaries:null,
-    totalOversPlayed:null,
-    totalOversBowled:null,
-    totalWickets:null,
+    totalBoundaries: null,
+    totalOversPlayed: null,
+    totalOversBowled: null,
+    totalWickets: null,
+    result: '',
   };
 
-  TeamAWickets: any= [];
+  TeamAWickets: any = [];
   constructor(private modalService: NgbModal) { }
-  teamAList:any=[];
-  teamBList:any=[];
+  teamAList: any = [];
+  teamBList: any = [];
   addFieldValue() {
     this.teamAList.push({
-      playerName :'',
-      runs:null,
-      boundaries:null,
+      playerName: '',
+      runs: null,
+      boundaries: null,
       wickets: null,
-      oversPlayed:null,
-      oversBowled:null,
+      oversPlayed: null,
+      oversBowled: null,
     })
   }
   deleteFieldValue(index) {
@@ -58,20 +60,20 @@ export class ScoreCardCalculatorComponent implements OnInit {
   }
   addFieldValueB() {
     this.teamBList.push({
-      playerName :'',
-      runs:null,
-      boundaries:null,
+      playerName: '',
+      runs: null,
+      boundaries: null,
       wickets: null,
-      oversPlayed:null,
-      oversBowled:null,
+      oversPlayed: null,
+      oversBowled: null,
     })
   }
   deleteFieldValueB(index) {
     this.teamBList.splice(index, 1);
   }
-  addTeamA(form){
-    if(form.invalid) return;
-    this.teamA.teamA=this.teamAList;
+  addTeamA(form) {
+    if (form.invalid) return;
+    this.teamA.teamA = this.teamAList;
     // console.log(this.teamA.teamA);
     this.totalTeamAWickets(this.teamA.teamA);
     this.totalTeamARuns(this.teamA.teamA);
@@ -79,9 +81,9 @@ export class ScoreCardCalculatorComponent implements OnInit {
     this.totalTeamABoundaries(this.teamA.teamA);
     this.totalTeamAOversBowled(this.teamA.teamA);
   }
-  addTeamB(form){
-    if(form.invalid) return;
-    this.teamB.teamB=this.teamBList;
+  addTeamB(form) {
+    if (form.invalid) return;
+    this.teamB.teamB = this.teamBList;
     console.log(this.teamB.teamB);
     this.totalTeamBWickets(this.teamB.teamB);
     this.totalTeamBRuns(this.teamB.teamB);
@@ -90,83 +92,104 @@ export class ScoreCardCalculatorComponent implements OnInit {
     this.totalTeamBOversBowled(this.teamB.teamB);
   }
   // For Team A calculations
-  totalTeamAWickets(array){
+  totalTeamAWickets(array) {
     // console.log(array);
-    var result = array.map(function(a) {return a.wickets;});
+    var result = array.map(function (a) { return a.wickets; });
     // console.log(result);
-    this.teamA.totalWickets = result.reduce(function(total, num){
+    this.teamA.totalWickets = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Num of Wickets ' + this.teamA.totalWickets);
   }
-  totalTeamARuns(array){
-    var result = array.map(function(a) {return a.runs;});
-    this.teamA.totalRuns = result.reduce(function(total, num){
+  totalTeamARuns(array) {
+    var result = array.map(function (a) { return a.runs; });
+    this.teamA.totalRuns = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Team Runs ' + this.teamA.totalRuns);
   }
-  totalTeamAOversPlayed(array){
-    var result = array.map(function(a) {return a.oversPlayed;});
-    this.teamA.totalOversPlayed = result.reduce(function(total, num){
+  totalTeamAOversPlayed(array) {
+    var result = array.map(function (a) { return a.oversPlayed; });
+    this.teamA.totalOversPlayed = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Overs Played ' + this.teamA.totalOversPlayed);
   }
-  totalTeamAOversBowled(array){
-    var result = array.map(function(a) {return a.oversBowled;});
-    this.teamA.totalOversBowled = result.reduce(function(total, num){
+  totalTeamAOversBowled(array) {
+    var result = array.map(function (a) { return a.oversBowled; });
+    this.teamA.totalOversBowled = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Overs Bowled ' + this.teamA.totalOversBowled);
   }
-  totalTeamABoundaries(array){
-    var result = array.map(function(a) {return a.boundaries;});
-    this.teamA.totalBoundaries = result.reduce(function(total, num){
+  totalTeamABoundaries(array) {
+    var result = array.map(function (a) { return a.boundaries; });
+    this.teamA.totalBoundaries = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Boundaries Hit ' + this.teamA.totalBoundaries);
   }
 
   // For Team B calculations
-  totalTeamBWickets(array){
+  totalTeamBWickets(array) {
     // console.log(array);
-    var result = array.map(function(a) {return a.wickets;});
+    // taking out all the wickets by each player
+    var result = array.map(function (a) { return a.wickets; });
     // console.log(result);
-    this.teamB.totalWickets = result.reduce(function(total, num){
+    this.teamB.totalWickets = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Num of Wickets ' + this.teamB.totalWickets);
   }
-  totalTeamBRuns(array){
-    var result = array.map(function(a) {return a.runs;});
-    this.teamB.totalRuns = result.reduce(function(total, num){
+  totalTeamBRuns(array) {
+    var result = array.map(function (a) { return a.runs; });
+    this.teamB.totalRuns = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Team Runs ' + this.teamB.totalRuns);
   }
-  totalTeamBOversPlayed(array){
-    var result = array.map(function(a) {return a.oversPlayed;});
-    this.teamB.totalOversPlayed = result.reduce(function(total, num){
+  totalTeamBOversPlayed(array) {
+    var result = array.map(function (a) { return a.oversPlayed; });
+    this.teamB.totalOversPlayed = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Overs Played ' + this.teamB.totalOversPlayed);
   }
-  totalTeamBOversBowled(array){
-    var result = array.map(function(a) {return a.oversBowled;});
-    this.teamB.totalOversBowled = result.reduce(function(total, num){
+  totalTeamBOversBowled(array) {
+    var result = array.map(function (a) { return a.oversBowled; });
+    this.teamB.totalOversBowled = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Overs Bowled ' + this.teamB.totalOversBowled);
   }
-  totalTeamBBoundaries(array){
-    var result = array.map(function(a) {return a.boundaries;});
-    this.teamB.totalBoundaries = result.reduce(function(total, num){
+  totalTeamBBoundaries(array) {
+    var result = array.map(function (a) { return a.boundaries; });
+    this.teamB.totalBoundaries = result.reduce(function (total, num) {
       return total + num;
     });
     console.log('Total Boundaries Hit ' + this.teamB.totalBoundaries);
   }
   ngOnInit() {
   }
+  resultDecider(totalRunsA, totalRunsB, boundariesA, boundariesB) {
+    if (totalRunsA > totalRunsB) {
+      this.teamA.result = 'Winner!!!';
+      this.teamB.result = 'Looser!!!';
+    } else if (totalRunsA < totalRunsB) {
+      this.teamA.result = 'Looser!!!';
+      this.teamB.result = 'Winner!!!';
+    } else if (totalRunsA == totalRunsB) {
+      if (boundariesA > boundariesB) {
+        this.teamA.result = 'Winner!!!';
+        this.teamB.result = 'Looser!!!';
+      } else if (boundariesA < boundariesB) {
+        this.teamA.result = 'Looser!!!';
+        this.teamB.result = 'Winner!!!';
+      } else if (boundariesA == boundariesB) {
+        this.teamA.result = 'Play Super Over!!!';
+        this.teamB.result = 'Play Super Over!!!';
+      }
+    }
 
+  }
 }

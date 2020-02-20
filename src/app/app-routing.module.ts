088902ea4b1d26ed578c3df66fa9efab4f341ba7
saikulasekhar <sekharsairoyal@gmail.com>
2020-02-20 17:58:13 +0530

    updates to deploy production

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { ScoreCardModule } from './score-card/score-card.module';
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/score-card/calculator', pathMatch: 'full' , runGuardsAndResolvers: 'always'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{

d16ec80d51e038cd4d424b4e71bc4f6afce842ca
saikulasekhar <sekharsairoyal@gmail.com>
2020-03-02 15:41:28 +0530

    triying to deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    useHash: false,
+    useHash: true,
     onSameUrlNavigation: 'reload',
     enableTracing: false
   }),SharedModule,ScoreCardModule],
